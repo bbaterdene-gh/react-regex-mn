464069a8a41285007399784c208a348995368589
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-12-05 12:27:37 +0800

    default remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="text-red-200"
+          className="text-red-200 bg-[#1da1f1]"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
